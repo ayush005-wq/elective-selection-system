e012502ed046d32679953f6e9f518556ec5235cc
ayush-mohite <76149823+ayush005-wq@users.noreply.github.com>
2025-02-04 19:22:37 +0530

    Update Course.js

--- backend/models/Course.js
+++ backend/models/Course.js
@@ -1,1 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+  const Course = sequelize.define('Course', {
+    code: { type: DataTypes.STRING(20), unique: true },
+    name: DataTypes.STRING(200),
+    seats: DataTypes.INTEGER,
+    seats_filled: { type: DataTypes.INTEGER, defaultValue: 0 },
+    schedule: DataTypes.JSONB,
+    syllabus: DataTypes.ARRAY(DataTypes.TEXT)
+  });
 
+  Course.associate = models => {
+    Course.belongsTo(models.ElectiveGroup);
+    Course.belongsTo(models.User, { as: 'Teacher' });
+  };
+
+  return Course;
+};
